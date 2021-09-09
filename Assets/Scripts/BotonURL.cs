132bf4929caa24fc45e2683c272384d84cca7233
Oniel Santos Álvarez <59784349+Oniel1721@users.noreply.github.com>
2021-09-09 11:22:57 -0300

    Links

--- Assets/Scripts/BotonURL.cs
+++ Assets/Scripts/BotonURL.cs
@@ -5,27 +5,9 @@ using UnityEngine;
 public class BotonURL : MonoBehaviour
 {
 	
-	/*public void openWeb(string url){
+	public void openWeb(string url){
 		Application.OpenURL(url);
 	
-	}*/
-	
-	public string IG = "https://instagram.com/t_ecogroup?utm_medium=copy_link";
-	public string Tconet = "https://t-ecogroup.net/";
-	
-	public void URLIG(){
-		
-		Application.OpenURL(IG);
-	}
-	
-	public void ULRTco(){
-		
-		Application.OpenURL(Tconet);
-	}
-	
-	void LaAurora()
-	{
-		Application.OpenURL("https://www.laaurora.com.do/?lang=en");
 	}
 	
 }
